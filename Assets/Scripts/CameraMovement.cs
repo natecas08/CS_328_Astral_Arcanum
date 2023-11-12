4615422102fb96d63c7221b5b51cca4835fd1f32
natecas08 <nathangomez@nevada.unr.edu>
2023-11-11 23:13:25 -0800

    Camera tracking

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -5,15 +5,35 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour
 {
     public Transform followTransform;
-    /*// Start is called before the first frame update
+    public BoxCollider2D mapBounds;
+    
+    
+
+    private float xMin, xMax, yMin, yMax;
+    private float camX, camY;
+    private float camOrthoSize;
+    private float camRatio;
+    private Camera mainCam;
+
+    // Start is called before the first frame update
+
     void Start()
     {
-        
-    }*/
+        xMin = mapBounds.bounds.min.x;
+        yMin = mapBounds.bounds.min.y;
+        xMax = mapBounds.bounds.max.x;
+        yMax = mapBounds.bounds.max.y;
+
+        mainCam = GetComponent<Camera>();
+        camOrthoSize = mainCam.orthographicSize;
+        camRatio = (xMax + camOrthoSize) / 2.0f;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        this.transform.position = new Vector3(followTransform.position.x, followTransform.position.y, this.transform.position.z);
+        camY = Mathf.Clamp(followTransform.position.y, yMin + camOrthoSize, yMax - camOrthoSize);
+        camX = Mathf.Clamp(followTransform.position.x, xMin + camRatio, xMax - camRatio);
+        this.transform.position = new Vector3(camX, camY, this.transform.position.z);
     }
 }
