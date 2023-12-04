0a5bafaa533f612e3698c9d0768ccd72ae192063
natecas08 <nathangomez@nevada.unr.edu>
2023-12-03 23:57:24 -0800

    finished repair animations

---
+++ Assets/Scripts/Environment&World/Bridge_Repair.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bridge_Repair_Animation : MonoBehaviour
+{
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("repairSpell") && PlayerController.repairCasted)
+        {
+            animator.SetBool("isRepaired", true);
+        }
+    }
+
+}
