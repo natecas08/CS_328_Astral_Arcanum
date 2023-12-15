3ef7b839909854b67c646ffb99bdb5d228639d52
Elixa-4 <4emmac@gmail.com>
2023-12-15 13:06:58 -0800

    Revert "spell selector"
    This reverts commit a2abd95d0962e3de42d124f8081206a48462579b, reversing
    changes made to c662fe45739fc597a62eaa42a841c71cc50895c4.

--- Assets/Scripts/Enemies/Bosses/FinalBoss.cs
+++ Assets/Scripts/Enemies/Bosses/FinalBoss.cs
@@ -14,10 +14,11 @@ public enum finalBossState
 
 public class finalBossController : MonoBehaviour
 {
+    public static bool invincible = false;
+
     public static float health = 10;
     public float maxHealth = 10;
     public Slider healthBar;
-    public GameObject corruptionObject;
 
     List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
     public ContactFilter2D movementFilter;
@@ -122,12 +123,21 @@ public class finalBossController : MonoBehaviour
             StartCoroutine(playerBreak());
         }
 
-        if(other.gameObject.CompareTag("Fire Spell"))
+        if(!invincible)
         {
-            hit = true;
-            healthBar.value = health/maxHealth;
-            hit = false;
-        }
+            if (other.gameObject.CompareTag("Fire Spell"))
+            {
+                hit = true;
+                healthBar.value = health / maxHealth;
+                hit = false;
+            }
+            if (other.gameObject.CompareTag("Freeze Spell"))
+            {
+                hit = true;
+                healthBar.value = health / maxHealth;
+                hit = false;
+            }
+        }   
     }
 
     IEnumerator playerBreak()
@@ -140,7 +150,7 @@ public class finalBossController : MonoBehaviour
 
         rb.velocity = new Vector2(0f, 0f);
         //GetComponent<SpriteRenderer>().sprite = squishedSprite;
-        yield return new WaitForSeconds(3);
+        yield return new WaitForSeconds(6);
         //GetComponent<SpriteRenderer>().sprite = normalSprite;
         hit = false;
     }
@@ -149,15 +159,23 @@ public class finalBossController : MonoBehaviour
     {
         //death
         Destroy(rb.gameObject);
+
     }
 
     void sheild()
     {
+        invincible = true;
+        StartCoroutine(sheildTimer());
+    }
 
+    IEnumerator sheildTimer()
+    {
+        yield return new WaitForSeconds(5);
+        invincible = false;
     }
 
     void corruption()
     {
-        //Rigidbody2D rb = corruptionObject.GetComponent<Rigidbody2D>();
+
     }
 }
